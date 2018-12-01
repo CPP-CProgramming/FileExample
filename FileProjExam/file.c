7dd4aecb0f9e468466dad5ae64491956b5c99e87
neogeoss <neogeo-s@hanmail.net>
2018-12-01 23:23:27 +0900

    2nd commit

--- FileProjExam/file.c
+++ FileProjExam/file.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char name[20];
+	char score;
+} Student;
 
 int main(void) {
-	char s[20] = "Hello world";
+	int n=0, sum = 0;
+	char * arglist = "";
 	FILE *fp;
-	fp = fopen("temp.txt", "w");
-	fprintf(fp, "%s\n", s);
-	fclose(fp);
+	fp = fopen("input.txt", "r");
+	fscanf(fp, "%d", &n);
+	printf("%d\n", n);
+	Student *student = (Student*)malloc(sizeof(Student)*n);
+	for (int i = 0; i < n; i++) {
+		fscanf(fp, "%s %d", &((student + i)->name), &((student + i)->score));
+		printf("이름 %s, 성적: %d\n", (student+i)->name, (student+i)->score);
+	}
+
+	
+	system("pause");
 	return 0;
 }
